1f433104a7f208a1f130e5b497af8c421fa1ad74
chenhaoMac <943525359@qq.com>
2026-07-19 17:45:18 +0800

    feat: combine top matching RAG chunks

--- src/graph/index.ts
+++ src/graph/index.ts
@@ -3,14 +3,11 @@ import { ChatOllama } from "@langchain/ollama";
 import { StateGraph, START, END, MemorySaver } from "@langchain/langgraph";
 import type { GraphNode, ConditionalEdgeRouter } from "@langchain/langgraph";
 import { v4 as uuidv4 } from "uuid";
-import ollama from "ollama";
-import { index } from "../verctor/index.ts";
 import { AgentState } from "../state/global.ts";
-import pool from "../database/db.ts";
-import type { DocumentChunkRow } from "../tools/database.ts";
-import { AIMessage, HumanMessage } from "@langchain/core/messages";
+import { AIMessage } from "@langchain/core/messages";
 import { classifyIntent } from "./intent.ts";
 import { runtimeOptions } from "../config/runtime.ts";
+import { retriveNode } from "./retrive.ts";
 
 const model = new ChatOllama({
     model: runtimeOptions.model,
@@ -40,39 +37,6 @@ const shouldContinue: ConditionalEdgeRouter<
     }
     return END;
 }
-
-const retriveNode: GraphNode<typeof AgentState> = async (state) => {
-    const intentEmbedding = await ollama.embed({
-        model: 'bge-m3',
-        input: state.question,
-    });
-    const searchResults = await index.query({
-        vector: intentEmbedding.embeddings[0], // 拿着问题的向量去数据库里比对
-        topK: 3,                // 只返回最相似的 1 条结果
-        includeMetadata: true   // 🚨 必须设为 true，不然它只把 1024 个数字还给你，你根本不知道原文是什么
-    });
-    const topMatch = searchResults.matches[0];
-    let documentDetail = null;
-    try {
-        const sql = `
-          SELECT * FROM document_chunks 
-          WHERE chunk_index = ?
-        `;
-        
-        // 传入泛型 <DocumentChunkRow[]>，确保 rows 有类型提示
-        const [rows] = await pool.query<DocumentChunkRow[]>(sql, [topMatch?.metadata?.chunk_index]);
-    
-        // 如果查到了，返回第一条；如果没查到，返回 null
-        [documentDetail] = rows;
-        console.log('@查到相关数据了', documentDetail);
-    } catch (error) {
-        console.error('查询切片数据失败:', error);
-        throw error;
-    }
-    return {
-        context: documentDetail?.content,
-    };
-};
 
 const agentBuilder = new StateGraph(AgentState)
     .addNode('chatNode', async (state) => {
